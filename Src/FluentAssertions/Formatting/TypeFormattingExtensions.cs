7c1bce95edcb6e12322a9173e17d664ea5409e0d
Christoph Bersch <2921215+cbersch@users.noreply.github.com>
2025-03-18 11:08:34 +0100

    Update Src/FluentAssertions/Formatting/TypeFormattingExtensions.cs
    Co-authored-by: ITaluone <44049228+ITaluone@users.noreply.github.com>

--- Src/FluentAssertions/Formatting/TypeFormattingExtensions.cs
+++ Src/FluentAssertions/Formatting/TypeFormattingExtensions.cs
@@ -8,7 +8,7 @@ internal static class TypeFormattingExtensions
     /// Gets a type which can be formatted to a type definition like Dictionary&lt;TKey, TValue&gt;
     /// </summary>
     /// <param name="type">The original type</param>
-    /// <returns>The type's type definition, it generic, or the original type.</returns>
+    /// <returns>The type's type definition if generic, or the original type.</returns>
     public static Type ToFormattableTypeDefinition(this Type type) =>
         type.IsGenericType && !type.IsGenericTypeDefinition ? type.GetGenericTypeDefinition() : type;
 }
