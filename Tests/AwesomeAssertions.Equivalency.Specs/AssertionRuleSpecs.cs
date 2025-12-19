2ea0900fe7d2d2fdadb6e204e7ff468117e25ca6
Lukas Grützmacher <github@lg2.de>
2025-12-19 23:29:14 +0100

    Update "because" and "AAA comment" pattern in Equivalency.Specs

--- Tests/AwesomeAssertions.Equivalency.Specs/AssertionRuleSpecs.cs
+++ Tests/AwesomeAssertions.Equivalency.Specs/AssertionRuleSpecs.cs
@@ -12,56 +12,42 @@ public class AssertionRuleSpecs
     [Fact]
     public void When_two_objects_have_the_same_property_values_it_should_succeed()
     {
-        // Arrange
         var subject = new { Age = 36, Birthdate = new DateTime(1973, 9, 20), Name = "Dennis" };
-
         var other = new { Age = 36, Birthdate = new DateTime(1973, 9, 20), Name = "Dennis" };
 
-        // Act / Assert
         subject.Should().BeEquivalentTo(other);
     }
 
     [Fact]
     public void When_two_objects_have_the_same_nullable_property_values_it_should_succeed()
     {
-        // Arrange
         var subject = new { Age = 36, Birthdate = (DateTime?)new DateTime(1973, 9, 20), Name = "Dennis" };
-
         var other = new { Age = 36, Birthdate = (DateTime?)new DateTime(1973, 9, 20), Name = "Dennis" };
 
-        // Act / Assert
         subject.Should().BeEquivalentTo(other);
     }
 
     [Fact]
     public void When_two_objects_have_the_same_properties_but_a_different_value_it_should_throw()
     {
-        // Arrange
         var subject = new { Age = 36 };
-
         var expectation = new { Age = 37 };
 
-        // Act
-        Action act = () => subject.Should().BeEquivalentTo(expectation, "because {0} are the same", "they");
+        Action act = () => subject.Should().BeEquivalentTo(expectation, "we want to test the {0} message", "failure");
 
-        // Assert
         act.Should().Throw<XunitException>().WithMessage(
-            "Expected*Age*to be 37 because they are the same, but found 36*");
+            "Expected*Age*to be 37 because we want to test the failure message, but found 36*");
     }
 
     [Fact]
     public void
         When_subject_has_a_valid_property_that_is_compared_with_a_null_property_it_should_throw_with_descriptive_message()
     {
-        // Arrange
         var subject = new { Name = "Dennis" };
-
         var other = new { Name = (string)null };
 
-        // Act
-        Action act = () => subject.Should().BeEquivalentTo(other, "we want to test the failure {0}", "message");
+        Action act = () => subject.Should().BeEquivalentTo(other, "we want to test the {0} message", "failure");
 
-        // Assert
         act.Should().Throw<XunitException>().WithMessage(
             "Expected property subject.Name to be <null>*we want to test the failure message*, but found \"Dennis\"*");
     }
@@ -69,45 +55,32 @@ public class AssertionRuleSpecs
     [Fact]
     public void When_two_collection_properties_dont_match_it_should_throw_and_specify_the_difference()
     {
-        // Arrange
         var subject = new { Values = new[] { 1, 2, 3 } };
-
         var other = new { Values = new[] { 1, 4, 3 } };
 
-        // Act
         Action act = () => subject.Should().BeEquivalentTo(other);
 
-        // Assert
-        act.Should().Throw<XunitException>().WithMessage(
-            "Expected*Values[1]*to be 4, but found 2*");
+        act.Should().Throw<XunitException>().WithMessage("Expected*Values[1]*to be 4, but found 2*");
     }
 
     [Fact]
     [SuppressMessage("ReSharper", "StringLiteralTypo")]
     public void When_two_string_properties_do_not_match_it_should_throw_and_state_the_difference()
     {
-        // Arrange
         var subject = new { Name = "Dennes" };
-
         var other = new { Name = "Dennis" };
 
-        // Act
         Action act = () => subject.Should().BeEquivalentTo(other, options => options.Including(d => d.Name));
 
-        // Assert
-        act.Should().Throw<XunitException>().WithMessage(
-            "Expected*Name to be*index 4*\"Dennes\"*\"Dennis\"*");
+        act.Should().Throw<XunitException>().WithMessage("Expected*Name to be*index 4*\"Dennes\"*\"Dennis\"*");
     }
 
     [Fact]
     public void When_two_properties_are_of_derived_types_but_are_equal_it_should_succeed()
     {
-        // Arrange
         var subject = new { Type = new DerivedCustomerType("123") };
-
         var other = new { Type = new CustomerType("123") };
 
-        // Act / Assert
         subject.Should().BeEquivalentTo(other);
     }
 
@@ -115,62 +88,42 @@ public class AssertionRuleSpecs
     public void
         When_two_properties_have_the_same_declared_type_but_different_runtime_types_and_are_equivalent_according_to_the_declared_type_it_should_succeed()
     {
-        // Arrange
         var subject = new { Type = (CustomerType)new DerivedCustomerType("123") };
-
         var other = new { Type = new CustomerType("123") };
 
-        // Act / Assert
         subject.Should().BeEquivalentTo(other);
     }
 
     [Fact]
     public void When_a_nested_property_is_equal_based_on_equality_comparer_it_should_not_throw()
     {
-        // Arrange
         var subject = new { Timestamp = 22.March(2020).At(19, 30) };
-
         var expectation = new { Timestamp = 1.January(2020).At(7, 31) };
 
-        // Act / Assert
-        subject.Should().BeEquivalentTo(expectation,
-            opt => opt.Using<DateTime, DateTimeByYearComparer>());
+        subject.Should().BeEquivalentTo(expectation, opt => opt.Using<DateTime, DateTimeByYearComparer>());
     }
 
     [Fact]
     public void When_a_nested_property_is_unequal_based_on_equality_comparer_it_should_throw()
     {
-        // Arrange
         var subject = new { Timestamp = 22.March(2020) };
-
         var expectation = new { Timestamp = 1.January(2021) };
 
-        // Act
-        Action act = () => subject.Should().BeEquivalentTo(expectation,
-            opt => opt.Using(new DateTimeByYearComparer()), "we want to test the {0} message", "failure");
+        Action act = () => subject.Should().BeEquivalentTo(expectation, opt => opt.Using(new DateTimeByYearComparer()), "we want to test the {0} message", "failure");
 
-        // Assert
-        act.Should()
-            .Throw<XunitException>()
-            .WithMessage("Expected*equal*2021*DateTimeByYearComparer*failure message*2020*");
+        act.Should().Throw<XunitException>().WithMessage("Expected*equal*2021*DateTimeByYearComparer*failure message*2020*");
     }
 
     [Fact]
     public void When_the_subjects_property_type_is_different_from_the_equality_comparer_it_should_throw()
     {
-        // Arrange
         var subject = new { Timestamp = 1L };
-
         var expectation = new { Timestamp = 1.January(2021) };
 
-        // Act
         Action act = () => subject.Should().BeEquivalentTo(expectation,
             opt => opt.Using(new DateTimeByYearComparer()), "we want to test the {0} message", "failure");
 
-        // Assert
-        act.Should()
-            .Throw<XunitException>()
-            .WithMessage("Expected*Timestamp*to be of type *DateTime*failure message*1L*");
+        act.Should().Throw<XunitException>().WithMessage("Expected*Timestamp*to be of type *DateTime*failure message*1L*");
     }
 
     private class DateTimeByYearComparer : IEqualityComparer<DateTime>
@@ -186,81 +139,51 @@ public class AssertionRuleSpecs
     [Fact]
     public void When_an_invalid_equality_comparer_is_provided_it_should_throw()
     {
-        // Arrange
         var subject = new { Timestamp = 22.March(2020) };
-
         var expectation = new { Timestamp = 1.January(2021) };
-
         IEqualityComparer<DateTime> equalityComparer = null;
 
-        // Act
-        Action act = () => subject.Should().BeEquivalentTo(expectation,
-            opt => opt.Using(equalityComparer));
+        Action act = () => subject.Should().BeEquivalentTo(expectation, opt => opt.Using(equalityComparer));
 
-        // Assert
-        act.Should()
-            .Throw<ArgumentNullException>()
-            .WithMessage("*comparer*");
+        act.Should().Throw<ArgumentNullException>().WithMessage("*comparer*");
     }
 
     [Fact]
     public void When_the_compile_time_type_does_not_match_the_equality_comparer_type_it_should_use_the_default_mechanics()
     {
-        // Arrange
         var subject = new { Property = (IInterface)new ConcreteClass("SomeString") };
-
         var expectation = new { Property = (IInterface)new ConcreteClass("SomeOtherString") };
 
-        // Act / Assert
-        subject.Should().BeEquivalentTo(expectation, opt =>
-            opt.Using<ConcreteClass, ConcreteClassEqualityComparer>());
+        subject.Should().BeEquivalentTo(expectation, opt => opt.Using<ConcreteClass, ConcreteClassEqualityComparer>());
     }
 
     [Fact]
     public void When_the_runtime_type_does_match_the_equality_comparer_type_it_should_use_the_default_mechanics()
     {
-        // Arrange
         var subject = new { Property = (IInterface)new ConcreteClass("SomeString") };
-
         var expectation = new { Property = (IInterface)new ConcreteClass("SomeOtherString") };
 
-        // Act
         Action act = () => subject.Should().BeEquivalentTo(expectation, opt => opt
             .PreferringRuntimeMemberTypes()
             .Using<ConcreteClass, ConcreteClassEqualityComparer>());
 
-        // Assert
         act.Should().Throw<XunitException>().WithMessage("*ConcreteClassEqualityComparer*");
     }
 
     [Fact]
     public void When_several_properties_do_not_match_they_are_all_reported_after_an_other_successful_assertion()
     {
-        // Arrange
-        var test = new Test
-        {
-            Id = 42,
-            Text = "some-text",
-        };
-        var actualTest = new Test
-        {
-            Id = 32,
-            Text = "some-other-text",
-        };
+        var test = new Test { Id = 42, Text = "some-text" };
+        var actualTest = new Test { Id = 32, Text = "some-other-text" };
 
-        // Act
         Action act = () =>
         {
             actualTest.Text.Should().Be("some-other-text");
             actualTest.Should().BeEquivalentTo(test);
         };
 
-        // Assert
         act.Should().Throw<XunitException>()
-            .WithMessage(
-                $"Expected property actualTest.Id to be*{Environment.NewLine}" +
-                "*Expected property actualTest.Text to be*"
-            );
+            .WithMessage($"Expected property actualTest.Id to be*{Environment.NewLine}*Expected property actualTest.Text to be*");
     }
 
     private sealed class Test
@@ -275,44 +198,32 @@ public class AssertionRuleSpecs
         [Fact]
         public void An_equality_comparer_of_non_nullable_type_is_not_invoked_on_nullable_member()
         {
-            // Arrange
             var subject = new { Timestamp = (DateTime?)22.March(2020) };
-
             var expectation = new { Timestamp = (DateTime?)1.January(2020) };
 
-            // Act
-            Action act = () => subject.Should().BeEquivalentTo(expectation,
-                opt => opt.Using(new DateTimeByYearComparer()));
+            Action act = () => subject.Should().BeEquivalentTo(expectation, opt => opt.Using(new DateTimeByYearComparer()));
 
-            // Assert
             act.Should().Throw<XunitException>().WithMessage("Expected*Timestamp*2020*");
         }
 
         [Fact]
         public void An_equality_comparer_of_nullable_type_is_not_invoked_on_non_nullable_member()
         {
-            // Arrange
             var subject = new { Timestamp = 22.March(2020) };
-
             var expectation = new { Timestamp = 1.January(2020) };
 
-            // Act
-            Action act = () => subject.Should().BeEquivalentTo(expectation,
-                opt => opt.Using(new NullableDateTimeByYearComparer()));
+            Action act = () =>
+                subject.Should().BeEquivalentTo(expectation, opt => opt.Using(new NullableDateTimeByYearComparer()));
 
-            // Assert
             act.Should().Throw<XunitException>().WithMessage("Expected*Timestamp*2020*");
         }
 
         [Fact]
         public void An_equality_comparer_of_non_nullable_type_is_invoked_on_non_nullable_data()
         {
-            // Arrange
             var subject = new { Timestamp = (DateTime?)22.March(2020) };
-
             var expectation = new { Timestamp = (DateTime?)1.January(2020) };
 
-            // Act
             subject.Should().BeEquivalentTo(expectation, opt => opt
                 .PreferringRuntimeMemberTypes()
                 .Using(new DateTimeByYearComparer()));
@@ -321,17 +232,13 @@ public class AssertionRuleSpecs
         [Fact]
         public void An_equality_comparer_of_nullable_type_is_not_invoked_on_non_nullable_data()
         {
-            // Arrange
             var subject = new { Timestamp = (DateTime?)22.March(2020) };
-
             var expectation = new { Timestamp = (DateTime?)1.January(2020) };
 
-            // Act
             Action act = () => subject.Should().BeEquivalentTo(expectation, opt => opt
                 .PreferringRuntimeMemberTypes()
                 .Using(new NullableDateTimeByYearComparer()));
 
-            // Assert
             act.Should().Throw<XunitException>().WithMessage("Expected*Timestamp*2020*");
         }
     }
