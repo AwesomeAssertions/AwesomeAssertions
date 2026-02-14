56d535231a21acd8969862d0c19ccce6bd96951f
Lukas Grützmacher <github@lg2.de>
2026-02-14 21:54:18 +0100

    Fix cloning of FormattingOptions

---
+++ Tests/AwesomeAssertions.Specs/Formatting/FormattingOptionsTests.cs
@@ -0,0 +1,18 @@
+﻿using AwesomeAssertions.Formatting;
+using Xunit;
+
+namespace AwesomeAssertions.Specs.Formatting;
+
+public class FormattingOptionsTests
+{
+    [Fact]
+    public void When_cloning_all_properties_should_be_copied()
+    {
+        var sut = new FormattingOptions();
+        sut.InitializeWithRandomData();
+
+        var clone = sut.Clone();
+
+        clone.Should().BeEquivalentTo(sut);
+    }
+}
---
+++ Tests/AwesomeAssertions.Specs/RandomDataExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace AwesomeAssertions.Specs;
+
+public static class RandomDataExtensions
+{
+    public static void InitializeWithRandomData(this object sut)
+    {
+        foreach (PropertyInfo propertyInfo in sut.GetType().GetProperties())
+        {
+            if (!propertyInfo.CanWrite)
+            {
+                continue;
+            }
+
+            if (propertyInfo.PropertyType == typeof(string))
+            {
+                propertyInfo.SetValue(sut, new Random().Next(100).ToString(CultureInfo.InvariantCulture));
+                continue;
+            }
+
+            if (propertyInfo.PropertyType == typeof(bool))
+            {
+                propertyInfo.SetValue(sut, new Random().Next(2) == 0);
+                continue;
+            }
+
+            propertyInfo.SetValue(sut, new Random().Next());
+        }
+    }
+}
