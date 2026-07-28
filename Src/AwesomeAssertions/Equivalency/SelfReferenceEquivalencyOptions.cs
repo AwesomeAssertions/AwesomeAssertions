50940f8011737f7fa329d273022d919916f4c9a4
Lukas Grützmacher <github@lg2.de>
2026-07-28 22:58:26 +0200

    Cosmetic fix

--- Src/AwesomeAssertions/Equivalency/SelfReferenceEquivalencyOptions.cs
+++ Src/AwesomeAssertions/Equivalency/SelfReferenceEquivalencyOptions.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using AwesomeAssertions.Common;
