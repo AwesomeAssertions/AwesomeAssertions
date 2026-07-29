af9d6c5c23453e32482ac81c91daccf1625b4322
Lukas Grützmacher <github@lg2.de>
2026-07-29 21:21:22 +0200

    Enable public contract

--- Tests/AwesomeAssertions.Specs/Exceptions/InvokingActionSpecs.cs
+++ Tests/AwesomeAssertions.Specs/Exceptions/InvokingActionSpecs.cs
@@ -3,33 +3,27 @@ using Xunit;
 
 namespace AwesomeAssertions.Specs.Exceptions;
 
+// ReSharper disable ReturnValueOfPureMethodIsNotUsed
+#pragma warning disable CA1806,MA0060 // false-positive, the result is used in the action
 public class InvokingActionSpecs
 {
     [Fact]
     public void Invoking_on_null_is_not_allowed()
     {
-        // Arrange
         Does someClass = null;
 
-        // Act
         Action act = () => someClass.Invoking(d => d.Do());
 
-        // Assert
-        act.Should().ThrowExactly<ArgumentNullException>()
-            .WithParameterName("subject");
+        act.Should().ThrowExactly<ArgumentNullException>().WithParameterName("subject");
     }
 
     [Fact]
     public void Invoking_with_null_is_not_allowed()
     {
-        // Arrange
         Does someClass = Does.NotThrow();
 
-        // Act
         Action act = () => someClass.Invoking(null);
 
-        // Assert
-        act.Should().ThrowExactly<ArgumentNullException>()
-            .WithParameterName("action");
+        act.Should().ThrowExactly<ArgumentNullException>().WithParameterName("action");
     }
 }
--- Tests/AwesomeAssertions.Specs/Exceptions/InvokingFunctionSpecs.cs
+++ Tests/AwesomeAssertions.Specs/Exceptions/InvokingFunctionSpecs.cs
@@ -3,33 +3,27 @@ using Xunit;
 
 namespace AwesomeAssertions.Specs.Exceptions;
 
+// ReSharper disable ReturnValueOfPureMethodIsNotUsed
+#pragma warning disable CA1806,MA0060 // false-positive, the result is used in the action
 public class InvokingFunctionSpecs
 {
     [Fact]
     public void Invoking_on_null_is_not_allowed()
     {
-        // Arrange
         Does someClass = null;
 
-        // Act
         Action act = () => someClass.Invoking(d => d.Return());
 
-        // Assert
-        act.Should().ThrowExactly<ArgumentNullException>()
-            .WithParameterName("subject");
+        act.Should().ThrowExactly<ArgumentNullException>().WithParameterName("subject");
     }
 
     [Fact]
     public void Invoking_with_null_is_not_allowed()
     {
-        // Arrange
         Does someClass = Does.NotThrow();
 
-        // Act
-        Action act = () => someClass.Invoking((Func<Does, object>)null);
+        Action act = () => someClass.Invoking<Does, object>(null);
 
-        // Assert
-        act.Should().ThrowExactly<ArgumentNullException>()
-            .WithParameterName("action");
+        act.Should().ThrowExactly<ArgumentNullException>().WithParameterName("action");
     }
 }
